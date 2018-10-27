7487efd775df90feffe9ae88b4768a11244bf468
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-10-28 00:01:36 +0100

    more stuff

---
+++ string.scala
@@ -0,0 +1,5 @@
+val anytext: String = "Francisco"
+
+print("the name was " + anytext + " or similar") // the name was Francisco or similar
+print(s"the name was $anytext or similar") // the name was Francisco or similar
+print(s"the name was ${anytext} or similar") // the name was Francisco or similar
