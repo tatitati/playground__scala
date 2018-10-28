37f91c6bd5c6169e7841a9a33e22c5bd22a5e317
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-10-28 17:44:59 +0000

    basic collections added

---
+++ collections.scala
@@ -0,0 +1,14 @@
+// lists
+val colors = List("blue" , "orange", "red")
+print(colors(0)) // blue
+print(colors(1)) // orange
+
+
+// sets
+val cities = Set("Madrid", "Barcelona", "Madrid", "Barcelona", "Zurich")
+print(cities) // Set(Madrid, Barcelona, Zurich)
+print(cities(1))
+
+val persons = Map("Australia" -> "Michael", "Spain" -> "Francisco", "Chinese" -> "Jin")
+print(persons) // Map(Australia -> Michael, Spain -> Francisco, Chinese -> Jin)
+print(persons("Australia")) // Michael
