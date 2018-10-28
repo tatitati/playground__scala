08a25b76b56b016411e24044bc3bc6c73c522fd9
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-10-28 18:11:41 +0000

    added lambadas

--- functions.scala
+++ functions.scala
@@ -4,3 +4,10 @@ def sayHello(to: String, from: String) {
 
 sayHello("francisco", "whoever") // Good morning francisco, from whoever
 sayHello(from = "whoever", to = "francisco") // Good morning francisco, from whoever
+
+
+//
+// Functions literals  or lambadas or anonymous functions
+//
+val helloTo = (name: String) => print(s"hello ${name} !!")
+helloTo("David") // hello David !!
