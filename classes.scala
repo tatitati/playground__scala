88e9f5e22209f74fcf3bce75435048d3053f974d
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-10-28 18:03:50 +0000

    more

--- classes.scala
+++ classes.scala
@@ -1,4 +1,8 @@
-class User(named: String, val age: Int = 34){
+//
+// Basic class
+//
+class User(named: String){
+    val age: Int = 34
     val name: String = named
     def greet() {
         print(s"Hi ${name}")
@@ -8,3 +12,17 @@ class User(named: String, val age: Int = 34){
 var user = new User("manolo")
 user.greet() // hi manolo
 print(user.age) // 34
+
+
+//
+// Extending a class with args in constructor
+//
+class BasicUser(named: String) extends User(named) {
+    override def greet() {
+        print(s"good bye ${name}")
+    }
+}
+
+
+var basicUser = new BasicUser(named = "Ricardo")
+print(basicUser.greet) // good by Ricardo
--- collections.scala
+++ collections.scala
@@ -4,11 +4,15 @@ print(colors(0)) // blue
 print(colors(1)) // orange
 
 
+
 // sets
 val cities = Set("Madrid", "Barcelona", "Madrid", "Barcelona", "Zurich")
 print(cities) // Set(Madrid, Barcelona, Zurich)
 print(cities(1))
 
+
+
+// maps
 val persons = Map("Australia" -> "Michael", "Spain" -> "Francisco", "Chinese" -> "Jin")
 print(persons) // Map(Australia -> Michael, Spain -> Francisco, Chinese -> Jin)
 print(persons("Australia")) // Michael
