2d7acfe8953d1b1dde484c7b5d7f8ec7b44dfab4
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-10-30 12:45:39 +0000

    more

--- types/tuples.scala
+++ types/tuples.scala
@@ -1,5 +1,11 @@
 val info = ("something here", 3434, false)
 
-print(info) // (something here,3434,false)
-print(info._1) // something here
-print(info._2) // 3434
+println(info) // (something here,3434,false)
+println(info._1) // something here
+println(info._2) // 3434
+
+
+// we can can unpack
+val (something, number1, number2) = ("something here", 3434, false)
+println(something)
+println(number1)
