1fde148b88c47e40e84815918cd16fe7798cc0c1
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-10-31 15:37:36 +0000

    added to

--- functions/loops.scala
+++ functions/loops.scala
@@ -7,3 +7,12 @@ println(0 until 12)// Range 0 until 12
 for( a <- 1 until 10){
   println(a ); // 1, 2....10
 }
+
+
+
+// to
+println(1 to 10) // range 1 to 10
+
+for (i <- 1 to 5)
+    println(i)
+
