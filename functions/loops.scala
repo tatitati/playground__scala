d65cef9788494947478b57096a7d8d8b80cc50ea
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-10-31 15:38:57 +0000

    better

--- functions/loops.scala
+++ functions/loops.scala
@@ -1,18 +1,14 @@
 
 //
-// Until
+// Until and to
 //
-println(0 until 12)// Range 0 until 12
-
-for( a <- 1 until 10){
-  println(a ); // 1, 2....10
-}
-
-
-
-// to
+println(0 until 12) // Range 0 until 12
 println(1 to 10) // range 1 to 10
 
-for (i <- 1 to 5)
-    println(i)
 
+
+for (i <- 1 to 5)
+    println(i) // 1, 2....5
+
+for( a <- 1 until 5)
+  println(a) // 1, 2....5
