6adb031a9d0c88e068a89b1ec7b44f1ad6f48079
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-10-31 16:12:34 +0000

    asdf

--- functions/functions.scala
+++ functions/functions.scala
@@ -43,6 +43,21 @@ println(number2) // 89.35
 
 
 //
+// implicit variables
+//
+// for multiple params in the function we have to group the implicit params
+def sayMsg2(msg: String)(implicit something: String): String = {
+    msg + something
+}
+println(sayMsg2("the post!!!")( "whatever")) // the post!!!whatever
+implicit var prefix2 = "this is an implicit prefix!!!"
+println(sayMsg2(msg = "this is the police")) // this is the policethis is an implicit prefix!!!
+
+
+
+
+
+//
 // Deprectated syntax: return values are ignored
 //
 def sum2(x: Int, y: Int) {
--- types/var_val.scala
+++ types/var_val.scala
@@ -1,7 +1,22 @@
+// val are constant
 val mynumber1: Int = 5
-print(mynumber1) // 5
+println(mynumber1) // 5
 
 
+// var can be modified
 var mynumber2: Int = 5
 mynumber2 = 1212
-print(mynumber2) // 1212
+println(mynumber2) // 1212
+
+
+//
+// computed variables
+//
+val number3: Int = 40;
+val myvariable: String =
+    if (number3 > 50)
+        "is bigger than 50"
+    else
+        "is less than 50"
+
+println(myvariable) // is less than 50
