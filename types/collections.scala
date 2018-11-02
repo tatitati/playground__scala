b28c29850bd5327fd5646917ee3316ae9e3224d8
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-11-02 14:16:12 +0000

    tail and head tested

--- types/collections.scala
+++ types/collections.scala
@@ -1,17 +1,20 @@
 // lists
 val colors = List("blue" , "orange", "red")
-print(colors(0)) // blue
-print(colors(1)) // orange
+println(colors(0)) // blue
+println(colors(1)) // orange
+
+println(colors.head) // blue
+println(colors.tail) // List(orange, red)
 
 
 
 // sets
 val cities = Set("Madrid", "Barcelona", "Madrid", "Barcelona", "Zurich")
-print(cities) // Set(Madrid, Barcelona, Zurich)
+println(cities) // Set(Madrid, Barcelona, Zurich)
 
 
 
 // maps
 val persons = Map("Australia" -> "Michael", "Spain" -> "Francisco", "Chinese" -> "Jin")
-print(persons) // Map(Australia -> Michael, Spain -> Francisco, Chinese -> Jin)
-print(persons("Australia")) // Michael
+println(persons) // Map(Australia -> Michael, Spain -> Francisco, Chinese -> Jin)
+println(persons("Australia")) // Michael
