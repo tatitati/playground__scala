69477cd52833b6c4354b6726723a1c262cb9570b
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-11-05 13:10:52 +0000

    asdf

--- functions/functions_invokations.scala
+++ functions/functions_invokations.scala
@@ -11,3 +11,12 @@ var val1 = val0 doubler 6
 var val2 = val0.doubler(6)
 println(val1) // 12
 println(val2) // 12
+
+
+
+def squarer(x: Int): Int = {
+	x * x
+}
+println(squarer(2)) // 4
+println(squarer{3+3}) // 36 "6*6"
+
