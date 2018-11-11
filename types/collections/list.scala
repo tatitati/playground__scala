c7e4b50e87e524ffc50445682cbe05ee3b6af15d
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-11-11 23:10:55 +0000

    added transofrmation from list to map

--- types/collections/list.scala
+++ types/collections/list.scala
@@ -7,3 +7,15 @@ println(colors.head) // blue
 println(colors.tail) // List(orange, red)
 
 
+//
+// convert list to map
+//
+case class Person(val age: Int, val name: String)
+
+val listPerson = List(
+	Person(32, "juan"),
+	Person(50, "antonio")
+)
+
+val mapPerson = listPerson.map(personItem => (personItem.name, personItem.age)).toMap
+println(mapPerson) // Map(juan -> 32, antonio -> 50)
