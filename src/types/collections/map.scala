3d7c5d11e569946771e108593e62772d0b8aaeff
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-11-12 11:14:40 +0000

    added a fucking question

--- src/types/collections/map.scala
+++ src/types/collections/map.scala
@@ -38,3 +38,10 @@ println(mymap.getOrElse("wrong", "city not found")) // city not found
 // default value
 mymap = Map("city" -> "Madrid").withDefaultValue("not found")
 println(mymap("wrong")) // not found
+
+
+
+//
+// Map is an immutable object, how can be this modified in the previous examples!?!??!?!?!?!?!?!?!?!?!?!?!??!?!?!?
+//
+//
