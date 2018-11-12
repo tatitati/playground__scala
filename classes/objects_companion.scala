878ca06a6e4882f88f766bc0a17d7f52c68aa2a6
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-11-12 09:59:33 +0000

    merged files

--- classes/objects_companion.scala
+++ classes/objects_companion.scala
@@ -1,4 +1,28 @@
 //
+// Object in scala de-couple static stuff from the traditional class
+//
+object Person1 {
+	def saySomething(something: String): String  = {
+		something
+	}
+}
+println(Person1.saySomething("this is something!!!")) // this is something!!!
+
+
+//
+// Apply as default method
+//
+object Person2 {
+	def apply(something: String): String  = {
+		something
+	}
+}
+println(Person2("this is something!!!")) // this is something!!!
+println(Person2 {"this is something!!!" + "blabla blablba"}) // this is something!!!blabla blablba
+
+
+
+//
 // Object as companion object to de-couple static methods
 //
 object Person3 {
@@ -14,7 +38,7 @@ println(Person3.saySomething("this is something!!!")) // this is something!!!
 
 
 //
-// Apply method as factory method
+// Apply method as factory method 
 //
 object Person4 {
 	def apply(age: Int): Person4  = {
@@ -28,5 +52,3 @@ class Person4(val age: Int, val name: String) {
 var myperson4 = Person4(33)
 println(myperson4.age) // 33
 println(myperson4.name) // default name
-
-
