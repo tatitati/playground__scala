ee1b272404c56ec30099872972a3388f98a8a908
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-11-12 09:55:56 +0000

    more

--- classes/objects_companion.scala
+++ classes/objects_companion.scala
@@ -1,27 +1,3 @@
-
-
-//
-// Object in scala de-couple static stuff from the traditional class
-//
-object Person1 {
-	def saySomething(something: String): String  = {
-		something
-	}
-}
-println(Person1.saySomething("this is something!!!")) // this is something!!!
-
-
-//
-// Apply as default method
-//
-object Person2 {
-	def apply(something: String): String  = {
-		something
-	}
-}
-println(Person2("this is something!!!")) // this is something!!!
-println(Person2 {"this is something!!!" + "blabla blablba"}) // this is something!!!blabla blablba
-
 //
 // Object as companion object to de-couple static methods
 //
