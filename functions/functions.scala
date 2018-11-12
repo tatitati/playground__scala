8ddb9bc5b1d347af82884bb1e440357e6190b287
Francisco Albert <francisco.albusac@bbc.co.uk>
2018-11-12 09:42:51 +0000

    Update functions.scala

--- functions/functions.scala
+++ functions/functions.scala
@@ -68,3 +68,39 @@ println(sum2(2, 3)) // ()
 
 
 
+//
+// Method invocation: Operator notation
+//
+class MyClass() {
+	def doubler(x: Int): Int = {
+		x * 2
+	}
+
+	def summer(x: Int, y: Int): Int = {
+		x + y
+	}
+}
+
+val val0 = new MyClass()
+var val1 = val0 doubler 6 // invokation of methodd with "operator notation" instead of the simple "."
+var val2 = val0.doubler(6)
+println(val1) // 12
+println(val2) // 12
+
+
+// "operator notation" but with multiple parameters
+var val3 = val0 summer (3,2)
+println(val3) // 5
+
+
+
+
+// Method invocation: Block expression
+def squarer(x: Int): Int = {
+	x * x
+}
+println(squarer(2)) // 4
+println(squarer{3+3}) // 36 "6*6"
+
+
+
