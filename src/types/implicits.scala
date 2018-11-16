087b04cb30394ac4ce7589cc8e49e8d360142147
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-11-16 20:08:24 +0000

    more

--- src/types/implicits.scala
+++ src/types/implicits.scala
@@ -5,7 +5,10 @@
 def sayMsg2(msg: String)(implicit something: String): String = {
 	msg + something
 }
+// we can pass both params
 println(sayMsg2("the post!!!")( "whatever")) // the post!!!whatever
+
+// we can "implicityly" pass the second parameter
 implicit var prefix2 = "this is an implicit prefix!!!"
 println(sayMsg2(msg = "this is the police")) // this is the policethis is an implicit prefix!!!
 
