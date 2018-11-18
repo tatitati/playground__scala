cb790b1c34b48665a75ef23b04794c6e655354e8
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-11-18 23:37:49 +0000

    added generics

--- src/classes/generics.scala
+++ src/classes/generics.scala
@@ -1,1 +1,54 @@
+trait Human {
+	def isFrom(): String
+}
 
+class African() extends Human {
+	def isFrom(): String = {
+		"I'm from Africa"
+	}
+}
+
+class Russian() extends Human {
+	def isFrom(): String = {
+		"I'm from Russia"
+	}
+}
+
+
+
+
+
+
+//
+// Upper bound. I can pass any subtime (or same) fro the upper specified class
+//
+class Foreigner0[A <: Human](foreigner: A) {
+	def isFrom(): String = {
+		foreigner.isFrom()
+	}
+}
+val african0 = new African()
+val foreigner0 = new Foreigner0[African](african0)
+println(foreigner0.isFrom())
+
+
+
+
+
+
+
+
+//
+// If I dont specify an upper bound, then I need to specify some "structural features", so that a function must exist
+//
+class Foreigner1[A <: { def isFrom(): String }](foreigner: A) {
+	def isFrom(): String = {
+		foreigner.isFrom()
+	}
+}
+
+
+
+val african1 = new African()
+val foreigner1 = new Foreigner1[African](african1)
+println(foreigner1.isFrom())
