d5a7314861e561fe417a0376e91a8593f45fc39c
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-12-11 00:32:03 +0000

    added control structures

---
+++ src/control_structures/loops.scala
@@ -0,0 +1,58 @@
+val mylist = List("myfirst", "mysecond", "mythird")
+
+//
+// For vs foreach
+//
+for(i <- mylist) println(i)
+// myfirst
+// mysecond
+// mythird
+mylist.foreach((i => println(i)))
+// myfirst
+// mysecond
+// mythird
+
+
+
+
+//
+// Guards: If vs withFilter
+//
+for{
+  i <- mylist
+  if i != "mysecond"
+} println(i)
+
+//myfirst
+//mythird
+
+
+mylist
+  .withFilter((i => i!="mysecond"))
+  .foreach((i => println(i)))
+
+//myfirst
+//mythird
+
+
+
+
+
+//
+// Yield vs map
+//
+val newlist = for(i <- mylist) yield i + "!!!"
+println(newlist) // List(myfirst!!!, mysecond!!!, mythird!!!)
+
+
+val newlist2 = mylist.map(i => i + "!!!")
+println(newlist2) // println(newlist) // List(myfirst!!!, mysecond!!!, mythird!!!)
+
+
+//
+// Mix
+//
+val newlist3 = mylist
+                .withFilter(i => i != "mysecond")
+                .map(i => i)
+println(newlist3) // List(myfirst, mythird)
