fc8b9596015e0ab5f721a8b187143411a867584f
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-12-18 23:33:05 +0000

    more

--- src/map.scala
+++ src/map.scala
@@ -1,5 +1,21 @@
 
 val listFruits = List("apple", "banana", "melon", "potato")
 
-var newlist = listFruits.map(_ => "whatever")
-println(newlist)
+var newlist1 = listFruits.map(_ + "!!!")
+println(newlist1) // List(apple!!!, banana!!!, melon!!!, potato!!!)
+
+
+
+
+
+var newlist2 = listFruits.map{
+  case x => "Found: " + x
+}
+println(newlist2) // List(Found: apple, Found: banana, Found: melon, Found: potato)
+
+
+
+
+
+var newlist3 = listFruits.map{ x => "Found: " + x}
+println(newlist3) // List(Found: apple, Found: banana, Found: melon, Found: potato)
