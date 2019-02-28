653fb8fd0b301660d098985ddc33684a5d70ef37
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-02-28 23:58:52 +0000

    more

--- src/functions/byname_arguments.scala
+++ src/functions/byname_arguments.scala
@@ -1,5 +1,22 @@
-
-
-def sum(a: Int, b: => Int): Int = {
-  
+//
+// First: using function-argument
+//
+def sum1(a: Int, b: (Int) => Int): Int = {
+    a + b(a)
 }
+
+assert(sum1(2,  (c: Int) => {c*10}) == 22)
+
+
+
+//
+// Second, using by-name arguments
+//
+
+
+def sum2(a: Int, b: => Int): Int = {
+  a + b
+}
+
+
+assert(sum2(2, 3 + 3) == 8)
